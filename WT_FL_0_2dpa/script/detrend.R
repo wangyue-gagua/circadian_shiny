6fc7dc3a5ca1f9f47be45949e84e15c652dce437
wangyue-gagua <2503308794@qq.com>
2022-11-12 09:20:26 +0800

    detrended venn plot

--- WT_FL_0_2dpa/script/detrend.R
+++ WT_FL_0_2dpa/script/detrend.R
@@ -6,7 +6,7 @@ my_cir_plot_WT_FL_0_2dpa("Ghir_D12G009360")
 
 write_csv(WT_FL_0_2day_TMM_sample_exp_detrended, "./mediumDataSave/WT_FL_0_2day_TMM_sample_exp_detrended.csv")
 WT_FL_0_2day_TMM_sample_exp_detrended <- fread("./mediumDataSave/WT_FL_0_2day_TMM_sample_exp_detrended.csv") %>%
- as_tibble()
+    as_tibble()
 
 # randomSixCluster1Genens <- c("Ghir_D12G020190", "Ghir_A12G005280", "Ghir_A13G013150", "Ghir_D07G006230", "Ghir_D02G019940", "Ghir_D08G002570")
 
@@ -69,63 +69,100 @@ FL_0_2day_genes_TMM_EXPR_mergeRep_selected_detrended <- FL_0_2day_genes_TMM_EXPR
 WT_0_2day_genes_TMM_EXPR_mergeRep_selected_detrended <- read_csv("./mediumDataSave/WT_0_2day_genes_TMM_EXPR_mergeRep_selected_detrended.csv")
 library(MetaCycle)
 meta2d(
-  infile = "./mediumDataSave/WT_0_2day_genes_TMM_EXPR_mergeRep_selected_detrended.csv",
-  filestyle = "csv",
-  timepoints = seq(1, 69, 4),
-  outdir = "WT_meta2d_detrended",
-  parallelize = TRUE,
-  nCores = 10,
-  minper = 24,
-  maxper = 24,
-  ARSdefaultPer = 24,
-  cycMethod = "JTK",
+    infile = "./mediumDataSave/WT_0_2day_genes_TMM_EXPR_mergeRep_selected_detrended.csv",
+    filestyle = "csv",
+    timepoints = seq(1, 69, 4),
+    outdir = "WT_meta2d_detrended",
+    parallelize = TRUE,
+    nCores = 10,
+    minper = 24,
+    maxper = 24,
+    ARSdefaultPer = 24,
+    cycMethod = "JTK",
 )
 
 meta2d(
-  infile = "./mediumDataSave/FL_0_2day_genes_TMM_EXPR_mergeRep_selected_detrended.csv",
-  filestyle = "csv",
-  timepoints = seq(1, 69, 4),
-  outdir = "FL_meta2d_detrended",
-  parallelize = TRUE,
-  nCores = 10,
-  minper = 24,
-  maxper = 24,
-  ARSdefaultPer = 24,
-  cycMethod = "JTK",
+    infile = "./mediumDataSave/FL_0_2day_genes_TMM_EXPR_mergeRep_selected_detrended.csv",
+    filestyle = "csv",
+    timepoints = seq(1, 69, 4),
+    outdir = "FL_meta2d_detrended",
+    parallelize = TRUE,
+    nCores = 10,
+    minper = 24,
+    maxper = 24,
+    ARSdefaultPer = 24,
+    cycMethod = "JTK",
 )
 
 WT_meta2d_detrended <- read_csv("WT_meta2d_detrended/JTKresult_WT_0_2day_genes_TMM_EXPR_mergeRep_selected_detrended.csv")
 FL_meta2d_detrended <- read_csv("FL_meta2d_detrended/JTKresult_FL_0_2day_genes_TMM_EXPR_mergeRep_selected_detrended.csv")
 
 plotRepCirca("Ghir_D04G020730", "GhMYB25")
-ggsave('figure/geneRepCirca/Ghir_D04G020730_GhMYB25.pdf', width = 10, height = 10)
+ggsave("figure/geneRepCirca/Ghir_D04G020730_GhMYB25.pdf", width = 10, height = 10)
 plotRepCirca("Ghir_D12G017660", "GhMYB25-like")
-ggsave('figure/geneRepCirca/Ghir_D12G017660_GhMYB25-like.pdf', width = 10, height = 10)
+ggsave("figure/geneRepCirca/Ghir_D12G017660_GhMYB25-like.pdf", width = 10, height = 10)
 my_cir_plot_WT_FL_0_2dpa("Ghir_D12G017660", "GhMYB25-like")
 plotRepCircaDetrended("Ghir_D12G017660", "GhMYB25-like")
-ggsave('figure/geneRepCirca/Ghir_D12G017660_GhMYB25-like_detrended.pdf', width = 10, height = 10)
+ggsave("figure/geneRepCirca/Ghir_D12G017660_GhMYB25-like_detrended.pdf", width = 10, height = 10)
 
 ## MYB25-like MYB25相位差
-WT_FL_0_2day_TMM_sample_exp %>% select(1:6, "Ghir_D04G020730", "Ghir_D12G017660") %>% 
-    pivot_longer(cols = c("Ghir_D04G020730", "Ghir_D12G017660"), names_to = "genes", values_to = "expression") %>% 
+WT_FL_0_2day_TMM_sample_exp %>%
+    select(1:6, "Ghir_D04G020730", "Ghir_D12G017660") %>%
+    pivot_longer(cols = c("Ghir_D04G020730", "Ghir_D12G017660"), names_to = "genes", values_to = "expression") %>%
     ggplot(aes(time, expression)) +
-        geom_point(aes(col = strain)) +
-        geom_smooth(aes(group = interaction(as.factor(replicate), strain, genes), color = strain, linetype = genes), span = 0.3) +
-        facet_wrap(~replicate, nrow = 2) +
-        scale_x_continuous(breaks = seq(1, 69, 4), ) +
-        geom_rect(
-            data = rects,
-            aes(
-                xmin = xstart,
-                xmax = xend,
-                ymin = 0,
-                ymax = Inf
-            ),
-            inherit.aes = FALSE,
-            alpha = 0.2
-        ) +
-        geom_vline(xintercept = c(6, 18), linetype = "dotted") +
-        labs(title = "Phase diff between MYB25 and MYB25-like", subtitle = NULL) +
-        ylab("relative expression/(TMM)") +
-        theme(axis.text.x = element_text(angle = 30, hjust = 1, vjust = 1))
-ggsave('figure/geneRepCirca/Ghir_D04G020730_Ghir_D12G017660_phaseDiff.pdf', width = 10, height = 10)
+    geom_point(aes(col = strain)) +
+    geom_smooth(aes(group = interaction(as.factor(replicate), strain, genes), color = strain, linetype = genes), span = 0.3) +
+    facet_wrap(~replicate, nrow = 2) +
+    scale_x_continuous(breaks = seq(1, 69, 4), ) +
+    geom_rect(
+        data = rects,
+        aes(
+            xmin = xstart,
+            xmax = xend,
+            ymin = 0,
+            ymax = Inf
+        ),
+        inherit.aes = FALSE,
+        alpha = 0.2
+    ) +
+    geom_vline(xintercept = c(6, 18), linetype = "dotted") +
+    labs(title = "Phase diff between MYB25 and MYB25-like", subtitle = NULL) +
+    ylab("relative expression/(TMM)") +
+    theme(axis.text.x = element_text(angle = 30, hjust = 1, vjust = 1))
+ggsave("figure/geneRepCirca/Ghir_D04G020730_Ghir_D12G017660_phaseDiff.pdf", width = 10, height = 10)
+
+## 查找Detrend后metacycle检测结果的变化
+WT_dat_detrended <- WT_meta2d_detrended %>% filter(BH.Q < 0.01)
+FL_dat_detrended <- FL_meta2d_detrended %>% filter(BH.Q < 0.01)
+WT_circ_genes_detrended <- WT_dat_detrended$CycID
+FL_circ_genes_detrended <- FL_dat_detrended$CycID
+
+WT_circ_genes_specific_detrended <- setdiff(WT_circ_genes_detrended, FL_circ_genes_detrended)
+FL_circ_genes_specific_detrended <- setdiff(FL_circ_genes_detrended, WT_circ_genes_detrended)
+WT_FL_circ_genes_common_detrended <- intersect(WT_circ_genes_detrended, FL_circ_genes_detrended)
+writeLines(WT_circ_genes_specific_detrended, "mediumDataSave/WT_circ_genes_specific_detrended.txt")
+writeLines(FL_circ_genes_specific_detrended, "mediumDataSave/FL_circ_genes_specific_detrended.txt")
+writeLines(WT_FL_circ_genes_common_detrended, "mediumDataSave/WT_FL_circ_genes_common_detrended.txt")
+
+library(VennDiagram)
+venn.diagram(
+    x = list(
+        WT_detrended = WT_circ_genes_detrended,
+        FL_detrended = FL_circ_genes_detrended,
+        WT = WT_circ_genes,
+        FL = FL_circ_genes
+    ),
+    col = "transparent",
+    fill = c("blue", "green", "yellow", "grey50"),
+    alpha = 0.50,
+    cex = 1.2, # 每个区域label名称的大小
+    cat.col = c("darkblue", "darkgreen", "orange", "grey50"), # 分类颜色
+    cat.cex = 1.2, # 每个分类名称大小
+    cat.dist = 0.07,
+    cat.pos = 0, #
+    cat.fontfamily = "serif", # 分类字体
+    rotation.degree = 270, # 旋转角度
+    margin = 0.2,
+    filename = "figure/samplesCor/circ_genes.png",
+    imagetype = "png",
+)
