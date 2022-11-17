1a091780e1b6b4caf10ef3e47058dbda93d085c1
wangyue-gagua <2503308794@qq.com>
2022-11-17 17:36:13 +0800

    按时间合并rmats中不同事件的IncLevel1，暂时完成WT SE

--- WT_FL_0_2dpa/script/alternativeSplicing.R
+++ WT_FL_0_2dpa/script/alternativeSplicing.R
@@ -9,3 +9,154 @@ p5 <- salmonPlotRepCirca("Ghir_A13G017740.1", "proline-rich spliceosome-associat
 
 p1 + p2 + p3 + p4 + p5 + plot_layout(ncol = 2, nrow = 3, guides = "collect")
 ggsave("figure/transcriptionPlot/PSP_5.pdf", width = 10, height = 10)
+
+# 0dpa SE
+rMATs_0DPA_9am_SE <- data.table::fread("/data1/wangy/circadian/rMATsMetaData/rMATs_output_0DPA_9am/SE.MATS.JC.txt") %>%
+    as_tibble(.name_repair = "unique") %>%
+    select(c(GeneID, exonStart_0base, exonEnd, upstreamES, upstreamEE, downstreamES, downstreamEE, IncLevel1, IncLevel2))
+rMATs_0DPA_1pm_SE <- data.table::fread("/data1/wangy/circadian/rMATsMetaData/rMATs_output_0DPA_1pm/SE.MATS.JC.txt") %>%
+    as_tibble(.name_repair = "unique") %>%
+    select(c(GeneID, exonStart_0base, exonEnd, upstreamES, upstreamEE, downstreamES, downstreamEE, IncLevel1, IncLevel2))
+rMATs_0DPA_5pm_SE <- data.table::fread("/data1/wangy/circadian/rMATsMetaData/rMATs_output_0DPA_5pm/SE.MATS.JC.txt") %>%
+    as_tibble(.name_repair = "unique") %>%
+    select(c(GeneID, exonStart_0base, exonEnd, upstreamES, upstreamEE, downstreamES, downstreamEE, IncLevel1, IncLevel2))
+rMATs_0DPA_9pm_SE <- data.table::fread("/data1/wangy/circadian/rMATsMetaData/rMATs_output_0DPA_9pm/SE.MATS.JC.txt") %>%
+    as_tibble(.name_repair = "unique") %>%
+    select(c(GeneID, exonStart_0base, exonEnd, upstreamES, upstreamEE, downstreamES, downstreamEE, IncLevel1, IncLevel2))
+rMATs_0DPA_1am_SE <- data.table::fread("/data1/wangy/circadian/rMATsMetaData/rMATs_output_0DPA_1am/SE.MATS.JC.txt") %>%
+    as_tibble(.name_repair = "unique") %>%
+    select(c(GeneID, exonStart_0base, exonEnd, upstreamES, upstreamEE, downstreamES, downstreamEE, IncLevel1, IncLevel2))
+rMATs_0DPA_5am_SE <- data.table::fread("/data1/wangy/circadian/rMATsMetaData/rMATs_output_0DPA_5am/SE.MATS.JC.txt") %>%
+    as_tibble(.name_repair = "unique") %>%
+    select(c(GeneID, exonStart_0base, exonEnd, upstreamES, upstreamEE, downstreamES, downstreamEE, IncLevel1, IncLevel2))
+
+# 1dpa SE
+rMATs_1DPA_9am_SE <- data.table::fread("/data1/wangy/circadian/rMATsMetaData/rMATs_output_1DPA_9am/SE.MATS.JC.txt") %>%
+    as_tibble(.name_repair = "unique") %>%
+    select(c(GeneID, exonStart_0base, exonEnd, upstreamES, upstreamEE, downstreamES, downstreamEE, IncLevel1, IncLevel2))
+rMATs_1DPA_1pm_SE <- data.table::fread("/data1/wangy/circadian/rMATsMetaData/rMATs_output_1DPA_1pm/SE.MATS.JC.txt") %>%
+    as_tibble(.name_repair = "unique") %>%
+    select(c(GeneID, exonStart_0base, exonEnd, upstreamES, upstreamEE, downstreamES, downstreamEE, IncLevel1, IncLevel2))
+rMATs_1DPA_5pm_SE <- data.table::fread("/data1/wangy/circadian/rMATsMetaData/rMATs_output_1DPA_5pm/SE.MATS.JC.txt") %>%
+    as_tibble(.name_repair = "unique") %>%
+    select(c(GeneID, exonStart_0base, exonEnd, upstreamES, upstreamEE, downstreamES, downstreamEE, IncLevel1, IncLevel2))
+rMATs_1DPA_9pm_SE <- data.table::fread("/data1/wangy/circadian/rMATsMetaData/rMATs_output_1DPA_9pm/SE.MATS.JC.txt") %>%
+    as_tibble(.name_repair = "unique") %>%
+    select(c(GeneID, exonStart_0base, exonEnd, upstreamES, upstreamEE, downstreamES, downstreamEE, IncLevel1, IncLevel2))
+rMATs_1DPA_1am_SE <- data.table::fread("/data1/wangy/circadian/rMATsMetaData/rMATs_output_1DPA_1am/SE.MATS.JC.txt") %>%
+    as_tibble(.name_repair = "unique") %>%
+    select(c(GeneID, exonStart_0base, exonEnd, upstreamES, upstreamEE, downstreamES, downstreamEE, IncLevel1, IncLevel2))
+rMATs_1DPA_5am_SE <- data.table::fread("/data1/wangy/circadian/rMATsMetaData/rMATs_output_1DPA_5am/SE.MATS.JC.txt") %>%
+    as_tibble(.name_repair = "unique") %>%
+    select(c(GeneID, exonStart_0base, exonEnd, upstreamES, upstreamEE, downstreamES, downstreamEE, IncLevel1, IncLevel2))
+
+# 2dpa SE
+rMATs_2DPA_9am_SE <- data.table::fread("/data1/wangy/circadian/rMATsMetaData/rMATs_output_2DPA_9am/SE.MATS.JC.txt") %>%
+    as_tibble(.name_repair = "unique") %>%
+    select(c(GeneID, exonStart_0base, exonEnd, upstreamES, upstreamEE, downstreamES, downstreamEE, IncLevel1, IncLevel2))
+rMATs_2DPA_1pm_SE <- data.table::fread("/data1/wangy/circadian/rMATsMetaData/rMATs_output_2DPA_1pm/SE.MATS.JC.txt") %>%
+    as_tibble(.name_repair = "unique") %>%
+    select(c(GeneID, exonStart_0base, exonEnd, upstreamES, upstreamEE, downstreamES, downstreamEE, IncLevel1, IncLevel2))
+rMATs_2DPA_5pm_SE <- data.table::fread("/data1/wangy/circadian/rMATsMetaData/rMATs_output_2DPA_5pm/SE.MATS.JC.txt") %>%
+    as_tibble(.name_repair = "unique") %>%
+    select(c(GeneID, exonStart_0base, exonEnd, upstreamES, upstreamEE, downstreamES, downstreamEE, IncLevel1, IncLevel2))
+rMATs_2DPA_9pm_SE <- data.table::fread("/data1/wangy/circadian/rMATsMetaData/rMATs_output_2DPA_9pm/SE.MATS.JC.txt") %>%
+    as_tibble(.name_repair = "unique") %>%
+    select(c(GeneID, exonStart_0base, exonEnd, upstreamES, upstreamEE, downstreamES, downstreamEE, IncLevel1, IncLevel2))
+rMATs_2DPA_1am_SE <- data.table::fread("/data1/wangy/circadian/rMATsMetaData/rMATs_output_2DPA_1am/SE.MATS.JC.txt") %>%
+    as_tibble(.name_repair = "unique") %>%
+    select(c(GeneID, exonStart_0base, exonEnd, upstreamES, upstreamEE, downstreamES, downstreamEE, IncLevel1, IncLevel2))
+rMATs_2DPA_5am_SE <- data.table::fread("/data1/wangy/circadian/rMATsMetaData/rMATs_output_2DPA_5am/SE.MATS.JC.txt") %>%
+    as_tibble(.name_repair = "unique") %>%
+    select(c(GeneID, exonStart_0base, exonEnd, upstreamES, upstreamEE, downstreamES, downstreamEE, IncLevel1, IncLevel2))
+
+# merge SE
+rMATs_merge_SE <- full_join(rMATs_0DPA_9am_SE, rMATs_0DPA_1pm_SE,
+    by = c("GeneID", "exonStart_0base", "exonEnd", "upstreamES", "upstreamEE", "downstreamES", "downstreamEE"), suffix = c("0DPA_9am", "0DPA_1pm")
+) %>%
+    full_join(rMATs_0DPA_5pm_SE,
+        by = c("GeneID", "exonStart_0base", "exonEnd", "upstreamES", "upstreamEE", "downstreamES", "downstreamEE"), suffix = c("0DPA_1pm", "0DPA_5pm")
+    ) %>%
+    full_join(rMATs_0DPA_9pm_SE,
+        by = c("GeneID", "exonStart_0base", "exonEnd", "upstreamES", "upstreamEE", "downstreamES", "downstreamEE"), suffix = c("0DPA_5pm", "0DPA_9pm")
+    ) %>%
+    full_join(rMATs_0DPA_1am_SE,
+        by = c("GeneID", "exonStart_0base", "exonEnd", "upstreamES", "upstreamEE", "downstreamES", "downstreamEE"), suffix = c("0DPA_9pm", "0DPA_1am")
+    ) %>%
+    full_join(rMATs_0DPA_5am_SE,
+        by = c("GeneID", "exonStart_0base", "exonEnd", "upstreamES", "upstreamEE", "downstreamES", "downstreamEE"), suffix = c("0DPA_1am", "0DPA_5am")
+    ) %>%
+    full_join(rMATs_1DPA_9am_SE,
+        by = c("GeneID", "exonStart_0base", "exonEnd", "upstreamES", "upstreamEE", "downstreamES", "downstreamEE"), suffix = c("0DPA_5am", "1DPA_9am")
+    ) %>%
+    full_join(rMATs_1DPA_1pm_SE,
+        by = c("GeneID", "exonStart_0base", "exonEnd", "upstreamES", "upstreamEE", "downstreamES", "downstreamEE"), suffix = c("1DPA_9am", "1DPA_1pm")
+    ) %>%
+    full_join(rMATs_1DPA_5pm_SE,
+        by = c("GeneID", "exonStart_0base", "exonEnd", "upstreamES", "upstreamEE", "downstreamES", "downstreamEE"), suffix = c("1DPA_1pm", "1DPA_5pm")
+    ) %>%
+    full_join(rMATs_1DPA_9pm_SE,
+        by = c("GeneID", "exonStart_0base", "exonEnd", "upstreamES", "upstreamEE", "downstreamES", "downstreamEE"), suffix = c("1DPA_5pm", "1DPA_9pm")
+    ) %>%
+    full_join(rMATs_1DPA_1am_SE,
+        by = c("GeneID", "exonStart_0base", "exonEnd", "upstreamES", "upstreamEE", "downstreamES", "downstreamEE"), suffix = c("1DPA_9pm", "1DPA_1am")
+    ) %>%
+    full_join(rMATs_1DPA_5am_SE,
+        by = c("GeneID", "exonStart_0base", "exonEnd", "upstreamES", "upstreamEE", "downstreamES", "downstreamEE"), suffix = c("1DPA_1am", "1DPA_5am")
+    ) %>%
+    full_join(rMATs_2DPA_9am_SE,
+        by = c("GeneID", "exonStart_0base", "exonEnd", "upstreamES", "upstreamEE", "downstreamES", "downstreamEE"), suffix = c("1DPA_5am", "2DPA_9am")
+    ) %>%
+    full_join(rMATs_2DPA_1pm_SE,
+        by = c("GeneID", "exonStart_0base", "exonEnd", "upstreamES", "upstreamEE", "downstreamES", "downstreamEE"), suffix = c("2DPA_9am", "2DPA_1pm")
+    ) %>%
+    full_join(rMATs_2DPA_5pm_SE,
+        by = c("GeneID", "exonStart_0base", "exonEnd", "upstreamES", "upstreamEE", "downstreamES", "downstreamEE"), suffix = c("2DPA_1pm", "2DPA_5pm")
+    ) %>%
+    full_join(rMATs_2DPA_9pm_SE,
+        by = c("GeneID", "exonStart_0base", "exonEnd", "upstreamES", "upstreamEE", "downstreamES", "downstreamEE"), suffix = c("2DPA_5pm", "2DPA_9pm")
+    ) %>%
+    full_join(rMATs_2DPA_1am_SE,
+        by = c("GeneID", "exonStart_0base", "exonEnd", "upstreamES", "upstreamEE", "downstreamES", "downstreamEE"), suffix = c("2DPA_9pm", "2DPA_1am")
+    ) %>%
+    full_join(rMATs_2DPA_5am_SE,
+        by = c("GeneID", "exonStart_0base", "exonEnd", "upstreamES", "upstreamEE", "downstreamES", "downstreamEE"), suffix = c("2DPA_1am", "2DPA_5am")
+    )
+
+## save middle file
+# write_csv(rMATs_merge_SE, "mediumDataSave/rMATs_merge_SE.csv")
+
+convertToDblAndMean <- function(str) {
+    if (is.na(str)) {
+        return(0)
+    } else {
+        a1 <- as.numeric(str_split(str, ",")[[1]][[1]])
+        a2 <- as.numeric(str_split(str, ",")[[1]][[2]])
+        means <- (a1 + a2) / 2
+        return(ifelse(is.na(means), 0, means))
+    }
+}
+rMATs_merge_SE_WT_exp <- rMATs_merge_SE %>%
+    select(-c(exonStart_0base, exonEnd, upstreamES, upstreamEE, downstreamES, downstreamEE)) %>%
+    mutate(
+        WT_0DPA_9am = map_dbl(IncLevel10DPA_9am, convertToDblAndMean),
+        WT_0DPA_1pm = map_dbl(IncLevel10DPA_1pm, convertToDblAndMean),
+        WT_0DPA_5pm = map_dbl(IncLevel10DPA_5pm, convertToDblAndMean),
+        WT_0DPA_9pm = map_dbl(IncLevel10DPA_9pm, convertToDblAndMean),
+        WT_0DPA_1am = map_dbl(IncLevel10DPA_1am, convertToDblAndMean),
+        WT_0DPA_5am = map_dbl(IncLevel10DPA_5am, convertToDblAndMean),
+        WT_1DPA_9am = map_dbl(IncLevel11DPA_9am, convertToDblAndMean),
+        WT_1DPA_1pm = map_dbl(IncLevel11DPA_1pm, convertToDblAndMean),
+        WT_1DPA_5pm = map_dbl(IncLevel11DPA_5pm, convertToDblAndMean),
+        WT_1DPA_9pm = map_dbl(IncLevel11DPA_9pm, convertToDblAndMean),
+        WT_1DPA_1am = map_dbl(IncLevel11DPA_1am, convertToDblAndMean),
+        WT_1DPA_5am = map_dbl(IncLevel11DPA_5am, convertToDblAndMean),
+        WT_2DPA_9am = map_dbl(IncLevel12DPA_9am, convertToDblAndMean),
+        WT_2DPA_1pm = map_dbl(IncLevel12DPA_1pm, convertToDblAndMean),
+        WT_2DPA_5pm = map_dbl(IncLevel12DPA_5pm, convertToDblAndMean),
+        WT_2DPA_9pm = map_dbl(IncLevel12DPA_9pm, convertToDblAndMean),
+        WT_2DPA_1am = map_dbl(IncLevel12DPA_1am, convertToDblAndMean),
+        WT_2DPA_5am = map_dbl(IncLevel12DPA_5am, convertToDblAndMean)
+    ) %>%
+    select(c(GeneID, starts_with("WT_")))
+# write_csv(rMATs_merge_SE_WT_exp, "mediumDataSave/rMATs_merge_SE_WT_exp.csv")
