4a37da54b6b730b45e6eee563396ceeb548a4276
wangyue-gagua <2503308794@qq.com>
2022-11-19 10:38:29 +0800

    GeneID去重再进行metacycle分析

--- WT_FL_0_2dpa/script/alternativeSplicingMetaCycle.R
+++ WT_FL_0_2dpa/script/alternativeSplicingMetaCycle.R
@@ -1,8 +1,9 @@
 library(MetaCycle)
 library(tidyverse)
-# 在metacycle分析之前，需要将NA值替换为0, 但不需要GeneID转换和去重
+# 在metacycle分析之前，需要将NA值替换为0, GeneID去重以便于下游分析
 ## A3SS WT
-rMATs_merge_A3SS_WT_exp <- read_csv("./mediumDataSave/rMATs_merge_A3SS_WT_exp.csv")
+rMATs_merge_A3SS_WT_exp <- read_csv("./mediumDataSave/rMATs_merge_A3SS_WT_exp.csv") %>% 
+    mutate(GeneID = str_c(GeneID, "_", seq_along(GeneID)))
 rMATs_merge_A3SS_WT_exp[is.na(rMATs_merge_A3SS_WT_exp)] <- 0
 write.csv(rMATs_merge_A3SS_WT_exp, "./mediumDataSave/removeNaMeta/rMATs_merge_A3SS_WT_exp.csv", row.names = FALSE)
 meta2d(
@@ -18,7 +19,8 @@ meta2d(
     cycMethod = c("JTK", "LS"),
 )
 ## A3SS FL
-rMATs_merge_A3SS_FL_exp <- read_csv("./mediumDataSave/rMATs_merge_A3SS_FL_exp.csv")
+rMATs_merge_A3SS_FL_exp <- read_csv("./mediumDataSave/rMATs_merge_A3SS_FL_exp.csv") %>% 
+    mutate(GeneID = str_c(GeneID, "_", seq_along(GeneID)))
 rMATs_merge_A3SS_FL_exp[is.na(rMATs_merge_A3SS_FL_exp)] <- 0
 write.csv(rMATs_merge_A3SS_FL_exp, "./mediumDataSave/removeNaMeta/rMATs_merge_A3SS_FL_exp.csv", row.names = FALSE)
 meta2d(
@@ -35,7 +37,8 @@ meta2d(
 )
 
 ## A5SS WT
-rMATs_merge_A5SS_WT_exp <- read_csv("./mediumDataSave/rMATs_merge_A5SS_WT_exp.csv")
+rMATs_merge_A5SS_WT_exp <- read_csv("./mediumDataSave/rMATs_merge_A5SS_WT_exp.csv") %>% 
+    mutate(GeneID = str_c(GeneID, "_", seq_along(GeneID)))
 rMATs_merge_A5SS_WT_exp[is.na(rMATs_merge_A5SS_WT_exp)] <- 0
 write.csv(rMATs_merge_A5SS_WT_exp, "./mediumDataSave/removeNaMeta/rMATs_merge_A5SS_WT_exp.csv", row.names = FALSE)
 meta2d(
@@ -51,7 +54,8 @@ meta2d(
     cycMethod = c("JTK", "LS"),
 )
 ## A5SS FL
-rMATs_merge_A5SS_FL_exp <- read_csv("./mediumDataSave/rMATs_merge_A5SS_FL_exp.csv")
+rMATs_merge_A5SS_FL_exp <- read_csv("./mediumDataSave/rMATs_merge_A5SS_FL_exp.csv") %>% 
+    mutate(GeneID = str_c(GeneID, "_", seq_along(GeneID)))
 rMATs_merge_A5SS_FL_exp[is.na(rMATs_merge_A5SS_FL_exp)] <- 0
 write.csv(rMATs_merge_A5SS_FL_exp, "./mediumDataSave/removeNaMeta/rMATs_merge_A5SS_FL_exp.csv", row.names = FALSE)
 meta2d(
@@ -67,7 +71,8 @@ meta2d(
     cycMethod = c("JTK", "LS"),
 )
 ## MXE WT
-rMATs_merge_MXE_WT_exp <- read_csv("./mediumDataSave/rMATs_merge_MXE_WT_exp.csv")
+rMATs_merge_MXE_WT_exp <- read_csv("./mediumDataSave/rMATs_merge_MXE_WT_exp.csv") %>% 
+    mutate(GeneID = str_c(GeneID, "_", seq_along(GeneID)))
 rMATs_merge_MXE_WT_exp[is.na(rMATs_merge_MXE_WT_exp)] <- 0
 write.csv(rMATs_merge_MXE_WT_exp, "./mediumDataSave/removeNaMeta/rMATs_merge_MXE_WT_exp.csv", row.names = FALSE)
 meta2d(
@@ -83,7 +88,8 @@ meta2d(
     cycMethod = c("JTK", "LS"),
 )
 ## MXE FL
-rMATs_merge_MXE_FL_exp <- read_csv("./mediumDataSave/rMATs_merge_MXE_FL_exp.csv")
+rMATs_merge_MXE_FL_exp <- read_csv("./mediumDataSave/rMATs_merge_MXE_FL_exp.csv") %>% 
+    mutate(GeneID = str_c(GeneID, "_", seq_along(GeneID)))
 rMATs_merge_MXE_FL_exp[is.na(rMATs_merge_MXE_FL_exp)] <- 0
 write.csv(rMATs_merge_MXE_FL_exp, "./mediumDataSave/removeNaMeta/rMATs_merge_MXE_FL_exp.csv", row.names = FALSE)
 meta2d(
@@ -99,7 +105,8 @@ meta2d(
     cycMethod = c("JTK", "LS"),
 )
 ## RI WT
-rMATs_merge_RI_WT_exp <- read_csv("./mediumDataSave/rMATs_merge_RI_WT_exp.csv")
+rMATs_merge_RI_WT_exp <- read_csv("./mediumDataSave/rMATs_merge_RI_WT_exp.csv") %>% 
+    mutate(GeneID = str_c(GeneID, "_", seq_along(GeneID)))
 rMATs_merge_RI_WT_exp[is.na(rMATs_merge_RI_WT_exp)] <- 0
 write.csv(rMATs_merge_RI_WT_exp, "./mediumDataSave/removeNaMeta/rMATs_merge_RI_WT_exp.csv", row.names = FALSE)
 meta2d(
@@ -115,7 +122,8 @@ meta2d(
     cycMethod = c("JTK", "LS"),
 )
 ## RI FL
-rMATs_merge_RI_FL_exp <- read_csv("./mediumDataSave/rMATs_merge_RI_FL_exp.csv")
+rMATs_merge_RI_FL_exp <- read_csv("./mediumDataSave/rMATs_merge_RI_FL_exp.csv") %>% 
+    mutate(GeneID = str_c(GeneID, "_", seq_along(GeneID)))
 rMATs_merge_RI_FL_exp[is.na(rMATs_merge_RI_FL_exp)] <- 0
 write.csv(rMATs_merge_RI_FL_exp, "./mediumDataSave/removeNaMeta/rMATs_merge_RI_FL_exp.csv", row.names = FALSE)
 meta2d(
@@ -131,7 +139,8 @@ meta2d(
     cycMethod = c("JTK", "LS"),
 )
 ## SE WT
-rMATs_merge_SE_WT_exp <- read_csv("./mediumDataSave/rMATs_merge_SE_WT_exp.csv")
+rMATs_merge_SE_WT_exp <- read_csv("./mediumDataSave/rMATs_merge_SE_WT_exp.csv") %>% 
+    mutate(GeneID = str_c(GeneID, "_", seq_along(GeneID)))
 rMATs_merge_SE_WT_exp[is.na(rMATs_merge_SE_WT_exp)] <- 0
 write.csv(rMATs_merge_SE_WT_exp, "./mediumDataSave/removeNaMeta/rMATs_merge_SE_WT_exp.csv", row.names = FALSE)
 meta2d(
@@ -147,7 +156,8 @@ meta2d(
     cycMethod = c("JTK", "LS"),
 )
 ## SE FL
-rMATs_merge_SE_FL_exp <- read_csv("./mediumDataSave/rMATs_merge_SE_FL_exp.csv")
+rMATs_merge_SE_FL_exp <- read_csv("./mediumDataSave/rMATs_merge_SE_FL_exp.csv") %>% 
+    mutate(GeneID = str_c(GeneID, "_", seq_along(GeneID)))
 rMATs_merge_SE_FL_exp[is.na(rMATs_merge_SE_FL_exp)] <- 0
 write.csv(rMATs_merge_SE_FL_exp, "./mediumDataSave/removeNaMeta/rMATs_merge_SE_FL_exp.csv", row.names = FALSE)
 meta2d(
