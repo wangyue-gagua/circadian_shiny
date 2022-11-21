9cf76ec2f8de2fd1275fe0ab32e4d62622eac6ef
wangyue-gagua <2503308794@qq.com>
2022-11-21 10:31:40 +0800

    Cycle alter Event: MXE RI SE pheatmap

--- WT_FL_0_2dpa/script/alternativeSplicingMetaAnalysis.R
+++ WT_FL_0_2dpa/script/alternativeSplicingMetaAnalysis.R
@@ -24,6 +24,7 @@ pheatmap(
     show_rownames = FALSE,
     show_colnames = TRUE,
     scale = "row",
+    main = "A3SS WT vs FL, N = 491",
     filename = "figure/alter/A3SS_WT_FL_cycle_exp.pdf"
 )
 
@@ -50,5 +51,87 @@ pheatmap(
     show_rownames = FALSE,
     show_colnames = TRUE,
     scale = "row",
+    main = "A5SS WT vs FL, N = 248",
     filename = "figure/alter/A5SS_WT_FL_cycle_exp.pdf"
+)
+
+## MXE WT
+rMATs_merge_MXE_WT_META2d <- read_csv("METACYCLE/rMATs_merge_MXE_WT_META2d/meta2d_rMATs_merge_MXE_WT_exp.csv")
+MXE_WT_cycle_genes <- rMATs_merge_MXE_WT_META2d %>% filter(meta2d_BH.Q < 0.01) %>% pull(CycID)
+rMATs_merge_MXE_WT_exp_NaRemoved <- read_csv("./mediumDataSave/removeNaMeta/rMATs_merge_MXE_WT_exp.csv")
+MXE_WT_cycle_exp <- rMATs_merge_MXE_WT_exp_NaRemoved %>% filter(GeneID %in% MXE_WT_cycle_genes)
+
+## MXE FL
+rMATs_merge_MXE_FL_META2d <- read_csv("METACYCLE/rMATs_merge_MXE_FL_META2d/meta2d_rMATs_merge_MXE_FL_exp.csv")
+MXE_FL_cycle_genes <- rMATs_merge_MXE_FL_META2d %>% filter(meta2d_BH.Q < 0.01) %>% pull(CycID)
+rMATs_merge_MXE_FL_exp_NaRemoved <- read_csv("./mediumDataSave/removeNaMeta/rMATs_merge_MXE_FL_exp.csv")
+MXE_FL_cycle_exp <- rMATs_merge_MXE_FL_exp_NaRemoved %>% filter(GeneID %in% MXE_FL_cycle_genes)
+
+### MXE WT vs FL
+MXE_WT_FL_cycle_exp <- full_join(MXE_WT_cycle_exp, MXE_FL_cycle_exp, by = "GeneID", suffix = c("_WT", "_FL")) %>%
+column_to_rownames("GeneID")
+MXE_WT_FL_cycle_exp[is.na(MXE_WT_FL_cycle_exp)] <- 0
+pheatmap(
+    MXE_WT_FL_cycle_exp,
+    cluster_cols = FALSE,
+    cluster_rows = FALSE,
+    show_rownames = FALSE,
+    show_colnames = TRUE,
+    scale = "row",
+    main = "MXE WT vs FL, N = 1",
+    filename = "figure/alter/MXE_WT_FL_cycle_exp.pdf"
+)
+
+## RI WT
+rMATs_merge_RI_WT_META2d <- read_csv("METACYCLE/rMATs_merge_RI_WT_META2d/meta2d_rMATs_merge_RI_WT_exp.csv")
+RI_WT_cycle_genes <- rMATs_merge_RI_WT_META2d %>% filter(meta2d_BH.Q < 0.01) %>% pull(CycID)
+rMATs_merge_RI_WT_exp_NaRemoved <- read_csv("./mediumDataSave/removeNaMeta/rMATs_merge_RI_WT_exp.csv")
+RI_WT_cycle_exp <- rMATs_merge_RI_WT_exp_NaRemoved %>% filter(GeneID %in% RI_WT_cycle_genes)
+
+## RI FL
+rMATs_merge_RI_FL_META2d <- read_csv("METACYCLE/rMATs_merge_RI_FL_META2d/meta2d_rMATs_merge_RI_FL_exp.csv")
+RI_FL_cycle_genes <- rMATs_merge_RI_FL_META2d %>% filter(meta2d_BH.Q < 0.01) %>% pull(CycID)
+rMATs_merge_RI_FL_exp_NaRemoved <- read_csv("./mediumDataSave/removeNaMeta/rMATs_merge_RI_FL_exp.csv")
+RI_FL_cycle_exp <- rMATs_merge_RI_FL_exp_NaRemoved %>% filter(GeneID %in% RI_FL_cycle_genes)
+
+### RI WT vs FL
+RI_WT_FL_cycle_exp <- full_join(RI_WT_cycle_exp, RI_FL_cycle_exp, by = "GeneID", suffix = c("_WT", "_FL")) %>%
+column_to_rownames("GeneID")
+RI_WT_FL_cycle_exp[is.na(RI_WT_FL_cycle_exp)] <- 0
+pheatmap(
+    RI_WT_FL_cycle_exp,
+    cluster_cols = FALSE,
+    # cluster_rows = FALSE,
+    show_rownames = FALSE,
+    show_colnames = TRUE,
+    scale = "row",
+    main = "RI WT vs FL, N = 775",
+    filename = "figure/alter/RI_WT_FL_cycle_exp.pdf"
+)
+
+## SE WT
+rMATs_merge_SE_WT_META2d <- read_csv("METACYCLE/rMATs_merge_SE_WT_META2d/meta2d_rMATs_merge_SE_WT_exp.csv")
+SE_WT_cycle_genes <- rMATs_merge_SE_WT_META2d %>% filter(meta2d_BH.Q < 0.01) %>% pull(CycID)
+rMATs_merge_SE_WT_exp_NaRemoved <- read_csv("./mediumDataSave/removeNaMeta/rMATs_merge_SE_WT_exp.csv")
+SE_WT_cycle_exp <- rMATs_merge_SE_WT_exp_NaRemoved %>% filter(GeneID %in% SE_WT_cycle_genes)
+
+## SE FL
+rMATs_merge_SE_FL_META2d <- read_csv("METACYCLE/rMATs_merge_SE_FL_META2d/meta2d_rMATs_merge_SE_FL_exp.csv")
+SE_FL_cycle_genes <- rMATs_merge_SE_FL_META2d %>% filter(meta2d_BH.Q < 0.01) %>% pull(CycID)
+rMATs_merge_SE_FL_exp_NaRemoved <- read_csv("./mediumDataSave/removeNaMeta/rMATs_merge_SE_FL_exp.csv")
+SE_FL_cycle_exp <- rMATs_merge_SE_FL_exp_NaRemoved %>% filter(GeneID %in% SE_FL_cycle_genes)
+
+### SE WT vs FL
+SE_WT_FL_cycle_exp <- full_join(SE_WT_cycle_exp, SE_FL_cycle_exp, by = "GeneID", suffix = c("_WT", "_FL")) %>%
+column_to_rownames("GeneID")
+SE_WT_FL_cycle_exp[is.na(SE_WT_FL_cycle_exp)] <- 0
+pheatmap(
+    SE_WT_FL_cycle_exp,
+    cluster_cols = FALSE,
+    # cluster_rows = FALSE,
+    show_rownames = FALSE,
+    show_colnames = TRUE,
+    scale = "row",
+    main = "SE WT vs FL, N = 461",
+    filename = "figure/alter/SE_WT_FL_cycle_exp.pdf"
 )
