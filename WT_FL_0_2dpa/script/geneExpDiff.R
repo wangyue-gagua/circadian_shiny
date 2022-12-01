e9b9ab0fcf6f13499dcda02a56c8df2c5516dfbd
wangyue-gagua <2503308794@qq.com>
2022-12-01 17:36:31 +0800

    格式化geneExpDiff.R

--- WT_FL_0_2dpa/script/geneExpDiff.R
+++ WT_FL_0_2dpa/script/geneExpDiff.R
@@ -1,19 +1,19 @@
 # 第一次测序与第二次测序检测基因数对比
-first_sample_info_exp <- data.table::fread("../merged_counts/sample_info_exp.csv") %>% 
-as_tibble()
+first_sample_info_exp <- data.table::fread("../merged_counts/sample_info_exp.csv") %>%
+    as_tibble()
 first_sample_list <- first_sample_info_exp %>%
-    group_by(strain) %>% 
+    group_by(strain) %>%
     arrange(time, .by_group = T) %>%
     pull(sample)
 first_count_matrix <- read_tsv("../merged_counts/genes.counts.matrix") %>%
-    rename(Geneid = `...1`) %>% 
+    rename(Geneid = `...1`) %>%
     select(Geneid, !!first_sample_list)
 
 second_sample_info_exp <- data.table::fread("../merged_counts/WT_FL_0_2day_TMM_sample_exp.csv") %>%
     as_tibble()
 second_sample_list <- second_sample_info_exp %>%
-    group_by(strain) %>% 
-    arrange(time, .by_group = T) %>% 
+    group_by(strain) %>%
+    arrange(time, .by_group = T) %>%
     pull(sample)
 
 
@@ -23,18 +23,18 @@ second_count_matrix <- read_tsv("../merged_counts/WT_FL_0_2day_genes.counts.matr
 
 
 first_count_matrix_longer <- first_count_matrix %>%
-    pivot_longer(cols = -Geneid, names_to = "sample", values_to = "count") %>% 
+    pivot_longer(cols = -Geneid, names_to = "sample", values_to = "count") %>%
     mutate(sample = factor(sample, levels = first_sample_list))
 
 second_count_matrix_longer <- second_count_matrix %>%
-    pivot_longer(cols = -Geneid, names_to = "sample", values_to = "count") %>% 
+    pivot_longer(cols = -Geneid, names_to = "sample", values_to = "count") %>%
     mutate(sample = factor(sample, levels = second_sample_list))
 
 stat_box_data <- function(y, upper_limit = log10(max(first_count_matrix_longer$count)) * 1.15) {
     return(
         data.frame(
             y = 0.95 * upper_limit,
-            label = length(y[10^y  > 3])
+            label = length(y[10^y > 3])
         )
     )
 }
@@ -76,5 +76,3 @@ second_count_matrix_longer_pdf <- second_count_matrix_longer %>% ggplot(aes(x = 
 
 ggsave("figure/geneExpDiff/second_count_matrix_longer.pdf", second_count_matrix_longer_pdf, width = 20, height = 4)
 
-first_count_matrix %>% summarise(across(where(is.numeric), ~ sum(.x > 3))) %>% as.numeric() %>% sd()
-second_count_matrix %>% summarise(across(where(is.numeric), ~ sum(.x > 3))) %>% as.numeric() %>% mean()
