45c19d8dfa6e29a3b6841cc2e2575e161a920f58
wangyue-gagua <2503308794@qq.com>
2022-12-10 00:47:06 +0800

    基因级联调控绘图

--- WT_FL_0_2dpa/script/phaseDiffRegulation.R
+++ WT_FL_0_2dpa/script/phaseDiffRegulation.R
@@ -49,9 +49,102 @@ WD40_ExpA1_cascading_regulation <- myb25CascadeGenesMtx %>%
     # geom_vline(xintercept = c(6, 18), linetype = "dotted") +
     labs(title = "Phase diff between WD40-EXPA1 cascading regulation", subtitle = NULL) +
     ylab("relative expression/(TMM)") +
-    facet_grid(cascadeLevel + factor(GhirAlias,
-     levels = c("WD40", "GhMYB2", "GhGL2", "GhMYB25-like", "GhMYB25", "GhHD-1", "GhHOX3-RT-F", "GhEXPASIN_A1", "GhLGO")) ~  replicate,
-      scales = "free_y") +
+    facet_grid(
+        cascadeLevel + factor(GhirAlias,
+            levels = c("WD40", "GhMYB2", "GhGL2", "GhMYB25-like", "GhMYB25", "GhHD-1", "GhHOX3-RT-F", "GhEXPASIN_A1", "GhLGO")
+        ) ~ replicate,
+        scales = "free_y"
+    ) +
     theme(axis.text.x = element_text(angle = 30, hjust = 1, vjust = 1))
 ggsave("figure/geneRepCirca/WD40_ExpA1_cascading_regulation.pdf", WD40_ExpA1_cascading_regulation, width = 10, height = 10)
+
+## 生长素相关基因
+auxinRelatedTibble <- tibble(
+    geneId = c(
+        "Ghir_A10G002560", "Ghir_D10G003340",
+        "Ghir_A05G002590", "Ghir_A06G020380", "Ghir_A09G024180", "Ghir_D05G002730", "Ghir_D05G004880", "Ghir_D09G023320",
+        "Ghir_A01G008830", "Ghir_A05G040190",
+        "Ghir_A03G022510", "Ghir_D13G010120"
+    ),
+    Families = c(
+        rep("ARF", 2),
+        rep("Aux/IAA", 6),
+        rep("GH3", 2),
+        rep("SAUR", 2)
+    )
+)
+auxinRelatedGeneList <- auxinRelatedTibble$geneId
+auxinRelatedGeneMtx <- WT_FL_0_2day_TMM_sample_exp %>%
+    dplyr::select(c(1:6, {{ auxinRelatedGeneList }})) %>%
+    pivot_longer(cols = starts_with("Ghir"), names_to = "genes", values_to = "expression") %>%
+    left_join(auxinRelatedTibble, by = c("genes" = "geneId"))
+
+auxinRelatedPlot <- auxinRelatedGeneMtx %>%
+    ggplot(aes(time, expression)) +
+    geom_point(aes(col = genes)) +
+    geom_smooth(aes(group = interaction(as.factor(replicate), strain, genes), color = genes, linetype = strain), span = 0.3) +
+    facet_wrap(~replicate, nrow = 2) +
+    scale_x_continuous(breaks = seq(1, 69, 4), ) +
+    geom_rect(
+        data = rects,
+        aes(
+            xmin = xstart,
+            xmax = xend,
+            ymin = 0,
+            ymax = Inf
+        ),
+        inherit.aes = FALSE,
+        alpha = 0.2
+    ) +
+    # geom_vline(xintercept = c(6, 18), linetype = "dotted") +
+    labs(title = "Phase diff between Auxin related cascading regulation", subtitle = NULL) +
+    ylab("relative expression/(TMM)") +
+    facet_grid(
+        Families + factor(genes,
+            levels = c(
+                "Ghir_A10G002560", "Ghir_D10G003340",
+                "Ghir_A05G002590", "Ghir_A06G020380", "Ghir_A09G024180", "Ghir_D05G002730", "Ghir_D05G004880", "Ghir_D09G023320",
+                "Ghir_A01G008830", "Ghir_A05G040190",
+                "Ghir_A03G022510", "Ghir_D13G010120"
+            )
+        ) ~ replicate,
+        scales = "free_y"
+    ) +
+    theme(axis.text.x = element_text(angle = 30, hjust = 1, vjust = 1))
+ggsave("figure/geneRepCirca/auxinRelatedPlot.pdf", auxinRelatedPlot, width = 10, height = 10)
+head(auxinRelatedGeneMtx)
+
+## normalize
+auxinRelatedGeneScaledMtx <-  WT_FL_0_2day_TMM_sample_exp %>%
+    dplyr::select(c(1:6, {{ auxinRelatedGeneList }}))  %>% 
+    mutate_if(is.double, scale) %>% 
+    pivot_longer(cols = starts_with("Ghir"), names_to = "genes", values_to = "expression") %>%
+    left_join(auxinRelatedTibble, by = c("genes" = "geneId"))
+
+auxinRelatedGeneScaledPlot <- auxinRelatedGeneScaledMtx %>% 
+    ggplot(aes(time, expression)) +
+    geom_point(aes(col = genes)) +
+    geom_smooth(aes(group = interaction(as.factor(replicate), strain, genes), color = genes, linetype = strain), span = 0.3) +
+    facet_wrap(~replicate, nrow = 2) +
+    scale_x_continuous(breaks = seq(1, 69, 4), ) +
+    geom_rect(
+        data = rects,
+        aes(
+            xmin = xstart,
+            xmax = xend,
+            ymin = -Inf,
+            ymax = Inf
+        ),
+        inherit.aes = FALSE,
+        alpha = 0.2
+    ) +
+    # geom_vline(xintercept = c(6, 18), linetype = "dotted") +
+    labs(title = "Phase diff between Auxin related cascading regulation", subtitle = NULL) +
+    ylab("Z-score of relative expression/(TMM)") +
+    facet_grid(
+        Families  ~ replicate,
+        scales = "free_y"
+    ) +
+    theme(axis.text.x = element_text(angle = 30, hjust = 1, vjust = 1))
+ggsave("figure/geneRepCirca/auxinRelatedGeneScaledPlot.pdf", auxinRelatedGeneScaledPlot, width = 10, height = 10)
 
