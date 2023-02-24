12982d94ad62464873d1f6903d104f3ebd52a3dd
wangyue-gagua <2503308794@qq.com>
2023-02-24 15:50:18 +0800

    去除离群值

--- WT_FL_0_2dpa/script/qPCR_validation.R
+++ WT_FL_0_2dpa/script/qPCR_validation.R
@@ -35,3 +35,45 @@ pRR5_qPCR_tbl_quant %>% ggplot(aes(timeString, means, color = strain)) +
     theme_bw()
 
 ggsave("figure/qPCR_validation/PRR5_2_5.pdf")
+
+# 去除离群值
+pRR5_qPCR_tbl_cq_normal <- pRR5_qPCR_tbl_cq %>%
+    mutate(
+        deDeltaCq = deltaCq - controlList,
+        quant = 2^(-deDeltaCq)
+    ) %>%
+    group_by(Sample, strain, day, phase) %>%
+    mutate(
+        upQuant = quantile(deltaCq, 0.75) + 1.5 * IQR(deltaCq),
+        downQuant = quantile(deltaCq, 0.25) - 1.5 * IQR(deltaCq),
+    ) %>%
+    filter(deltaCq < upQuant & deltaCq > downQuant)
+
+
+pRR5_qPCR_tbl_cq_filtered <- pRR5_qPCR_tbl_cq_normal %>%
+    ungroup() %>%
+    slice(-c(2, 6, 9, 11, 15, 18, 19, 22, 27, 28, 33, 34)) %>%
+    mutate(deltaCq = PRR5 - UBQ)
+
+slice(pRR5_qPCR_tbl_cq_normal, c(1, 28))
+
+filterCntList <- rep(pRR5_qPCR_tbl_cq_filtered$deltaCq[1:2], length(pRR5_qPCR_tbl_cq_filtered$deltaCq) / 2)
+
+pRR5_qPCR_tbl_cq_filtered_plot <- pRR5_qPCR_tbl_cq_filtered %>%
+    ungroup() %>%
+    mutate(
+        deDeltaCq = deltaCq - filterCntList,
+        quant = 2^(-deDeltaCq)
+    ) %>%
+    group_by(Sample, strain, day, phase) %>%
+    summarise(means = mean(quant), sd = sd(quant)) %>%
+    mutate(timeString = str_c(day, "Dpa", phase))
+
+pRR5_qPCR_tbl_cq_filtered_plot %>% ggplot(aes(timeString, means, color = strain)) +
+    geom_point() +
+    geom_errorbar(aes(ymin = means - sd, ymax = means + sd), width = 0.2) +
+    geom_line(aes(group = strain)) +
+    labs(x = "Time", y = "Relative Expression", title = "PRR5 qPCR validation") +
+    theme_bw()
+
+ggsave("figure/qPCR_validation/PRR5_2_5_filtered.pdf")
